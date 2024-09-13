9a304605aec88217ba51271c34e9999993f917d5
ericarieth <ericarieth@gmail.com>
2024-09-13 17:19:24 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -30,3 +30,26 @@ if __name__ == "__main__":
 git add hello.py
 git commit -m "Adiciona nova funcionalidade e configura workflows para CI e CD"
 git push origin nova-feature
+# Usar uma imagem base oficial do Python
+FROM python:3.10-slim
+
+# requirements.txt
+pytest
+requests
+# Define o diretório de trabalho dentro do container
+WORKDIR /app
+
+# Copia o arquivo de requisitos para o container
+COPY requirements.txt .
+
+# Instala as dependências listadas no requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copia o restante do código da aplicação para o container
+COPY . .
+
+# Define o comando que será executado quando o container iniciar
+CMD ["python", "hello.py"]
+
+docker build -t meu_projeto_python .
+docker run -it --rm meu_projeto_python
